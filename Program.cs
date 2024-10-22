2b76264ef7a4eb1f9b42742e950cafa0ce7693d8
Chris Gaudet <chrisgaudet9@gmail.com>
2024-10-22 18:58:12 -0500

    completed weather map exercise

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace WeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myKey = File.ReadAllText("appsettings.json");
+            string APIKey = (string)JObject.Parse(myKey).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please Enter Your Zip Code");
+            var cityName = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine("--------");
+
+            Console.WriteLine($"It's currently {WeatherMap.MyTemp(apiCall)} degrees Farenheit in your location");
+        }
+    }
+}
---
+++ WeatherMap.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMap
+{
+    public class WeatherMap
+    {
+        public static double MyTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
